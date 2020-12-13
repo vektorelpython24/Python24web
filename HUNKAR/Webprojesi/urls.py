71330e43a2dd44d36bc90f4e76aec1ba0a705602
İbrahim EDİZ <ibrahim.ediz@gazi.edu.tr>
2020-12-13 16:09:38 +0300

    13122020

--- HUNKAR/Webprojesi/urls.py
+++ HUNKAR/Webprojesi/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('blog.urls')),
-    path('',include('deneme.urls'))
+    path('blog/',include('blog.urls')),
+    path('deneme/',include('deneme.urls')),
 ]
