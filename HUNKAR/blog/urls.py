386142b5c915d164d1eb89cf2cfaf6230521ac3e
hunkargenchcttp <67922360+hunkargenchcttp@users.noreply.github.com>
2020-12-13 16:17:47 +0300

    commit

--- HUNKAR/blog/urls.py
+++ HUNKAR/blog/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.gonderiListe,name='gonderiListe')
+    path('gonderi/',views.gonderiListe,name='gonderiListe')
 ]
